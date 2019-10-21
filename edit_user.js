dbbe3be80270e2b8991ff23a9528302651cf85d8
Volodymyr <vintoniak7@gmail.com>
2019-10-21 20:51:50 +0300

    hot task with changing users profiles

--- edit_user.js
+++ edit_user.js
@@ -1,20 +1,36 @@
-
-
 $(document).ready(function(){
+var button;
+
+
+$('#myModal').on('show.bs.modal', function (event) {
+
+	button = $(event.relatedTarget) // Button that triggered the modal
+
+	var recipient = button.data('whatever') // Extract info from data-* attributes
+
+	// If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+
+	// Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
 	
-	$('#editbutton').on('click', function(){
-		
-		var value1, value2, value3;
-		
-		value1 = $('#name_mod').val();
-		$('#name1').html(value1);
-		
-		value2 = $('#address_mod').val();
-		$('#address1').html(value2);
-		
-		value3 = $('#mail_mod').val();
-		$('#mail1').html(value3);
-	});
+	var modal = $(this)
+
+	modal.find('.modal-title').text('New subscrider ' + recipient)
+
+	modal.find('.modal-body input').val(recipient)
+
+})
+
+$('#editbutton').on('click', function(){
+
+	button[0].parentNode.children[0].innerText = $("#name_mod")[0].value;
+
+	button[0].parentNode.children[2].innerText = $("#address_mod")[0].value;
+
+	button[0].parentNode.children[3].innerText = $("#mail_mod")[0].value;
+
+})
+	
+
 });
 
 
